ce7bfadf19e8e00af89e29b898245f9bd60050eb
vladimir <wertyxa@bk.ru>
2015-09-20 19:59:42 +0500

    normalize code

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,11 @@
-var MatrixActions = Reflux.createActions(['matrixError','matrixValid'])
+var MatrixActions = Reflux.createActions(['matrixError','matrixValid']);
 
 class Matrix {
     constructor(width, height){
-        this._width = width || 0
-        this._height = height || 0
-        this._array = null
-        this.$wrapper = $('<div class="wrapper-table" />')
-        this._isValid = true
+        this._width = width || 0;
+        this._height = height || 0;
+        this._array = null;
+        this.$wrapper = $('<div class="wrapper-table" ></div>');
     }
     _attachEvents(){}
     getWidth(){return this._width }
@@ -27,35 +26,35 @@ class Matrix {
         return this.$wrapper
     }
     readFromTable(){
-        var array = []
+        var array = [];
         this.$rows.each((ind, row)=>{
-            let subArray = []
+            let subArray = [];
             $(row).find(this.$inputs).each((ind, input)=>{
                 subArray.push($(input).val())
-            })
+            });
             array.push(subArray)
-        })
-        this.setValue(array)
+        });
+        this.setValue(array);
         return array
     }
     create(){
-        var $bracketRight = $('<div class="table__brackets-r" />')
-        var $bracketLeft = $('<div class="table__brackets-l" />')
-        this.$table = $('<div class="table" />')
+        var $bracketRight = $('<div class="table__brackets-r" />');
+        var $bracketLeft = $('<div class="table__brackets-l" />');
+        this.$table = $('<div class="table" />');
         for (var i = 0; i < this._height; i++) {
-            let $row = $('<div class="table__row" />')
+            let $row = $('<div class="table__row" />');
             for (var j = 0; j < this._width; j++) {
-                let $input = $('<input type="text" class="table__cell-input" />')
-                let $cell = $('<div class="table__cell" />')
-                $cell.append($input)
+                let $input = $('<input type="text" class="table__cell-input" />');
+                let $cell = $('<div class="table__cell" />');
+                $cell.append($input);
                 $row.append($cell)
-            };
+            }
             this.$table.append($row).appendTo(this.$wrapper)
-        };
-        this.$wrapper.append($bracketRight).prepend($bracketLeft)
-        this.$rows = this.$table.find('.table__row')
-        this.$inputs = this.$table.find('.table__cell-input')
-        this._attachEvents()
+        }
+        this.$wrapper.append($bracketRight).prepend($bracketLeft);
+        this.$rows = this.$table.find('.table__row');
+        this.$inputs = this.$table.find('.table__cell-input');
+        this._attachEvents();
         return this
     }
 
@@ -63,14 +62,13 @@ class Matrix {
 
 class InputMatrix extends Matrix {
     _attachEvents(){
-        var _this = this
+        var _this = this;
         this.$inputs.on('keyup',function(){
             _this._validate($(this))
         })
     }
     _showError($element){
-        $element.css('border','1px solid red')
-        this._isValid = false
+        $element.css('border','1px solid red');
         MatrixActions.matrixError()
     }
     _clearError($element){
@@ -80,7 +78,7 @@ class InputMatrix extends Matrix {
         if (isNaN($element.val())) {
             this._showError($element)
         } else {
-            this._clearError($element)
+            this._clearError($element);
             MatrixActions.matrixValid()
         }
     }
@@ -92,31 +90,30 @@ class OutputMatrix extends Matrix {
             $(row).find(this.$inputs).each((indInput, input)=>{
                 $(input).val(array[indRow][indInput])
             })
-        })
+        });
         return this
     }
 }
 
 var Operational = {
     sum(matrixesAsArray){
-        var newArray = []
-        matrixesAsArray.forEach((matrix, ind, matrixes)=>{
-            var lastMatrix = matrixes[ind-1]
-            if (ind !== 0 && matrix.length === lastMatrix.length 
-                && matrix[0].length === lastMatrix[0].length) {
-                for (var i = 0; i < matrix.length; i++) {
-                    newArray[i] = []
-                    for (var j = 0; j < matrix[i].length; j++) {
-                        newArray[i][j] = +matrix[i][j] + +lastMatrix[i][j]
-                    };
-                };
-            };
-        })
-        if (!newArray.length) throw new Error('Массивы разной величины или один массив')
+        var newArray = [];
+        var firstMatrix = _.first(matrixesAsArray);
+        var secondMatrix = _.last(matrixesAsArray);
+        if (secondMatrix.length === firstMatrix.length 
+            && secondMatrix[0].length === firstMatrix[0].length) {
+            for (var i = 0; i < secondMatrix.length; i++) {
+                newArray[i] = [];
+                for (var j = 0; j < secondMatrix[i].length; j++) {
+                    newArray[i][j] = +secondMatrix[i][j] + +firstMatrix[i][j]
+                }
+            }
+        }
+        if (!newArray.length) throw new Error('Массивы разной величины или один массив');
         return newArray
     },
     multiply(){}
-}
+};
 
 class Operator {
     constructor(symbol){
@@ -124,8 +121,8 @@ class Operator {
             '+':'sum',
             '-':'minus',
             '*':'multiply',
-            '/':'divide',
-        } 
+            '/':'divide'
+        }; 
         if (operators[symbol] !== undefined) {
             this._operator = operators[symbol]
         } else {
@@ -140,19 +137,19 @@ class Operator {
 
 class MatrixControl {
     constructor(matrixes, operator){
-        this._operator = operator
-        this._matrixes = matrixes || []
-        this._$calcField = $('#calcField')
-        this._$resultField = $('#resultField')
-        this._$button = $('#getResult')
-        this._disabled = false
+        this._operator = operator;
+        this._matrixes = matrixes || [];
+        this._$calcField = $('#calcField');
+        this._$resultField = $('#resultField');
+        this._$button = $('#getResult');
+        this._disabled = false;
 
-        this._init()
-        this._createStore()
-        this._attachEvents()
+        this._init();
+        this._createStore();
+        this._attachEvents();
     }
     _createStore(){
-        var _this = this
+        var _this = this;
         Reflux.createStore({
             listenables:MatrixActions,
             onMatrixError(){
@@ -164,35 +161,28 @@ class MatrixControl {
         })        
     }
     _checkTables(){
-        let isValid = true
-        this._matrixes.forEach(matrix=>{
-            matrix.$inputs.each((ind, input)=>{
-                if (isNaN($(input).val())) {
-                    isValid = false
-                    return false
-                }
+        _.every(this._matrixes, matrix=>{
+            return _.every(matrix.$inputs, input =>{
+                return !isNaN($(input).val())
             })
-        })
-        if (isValid) {
-            this._enable()
-        }
+        }) && this._enable()
     }
     _attachEvents(){
         //todo: enter key
-        this._$button.on('click', (evt)=>{
+        this._$button.on('click', ()=>{
             if (!this._disabled) this._calc()
         })
     }
     _disable(){
-        this._$button.addClass('disabled')
+        this._$button.addClass('disabled');
         this._disabled = true
     }
     _enable(){
-        this._$button.removeClass('disabled')
+        this._$button.removeClass('disabled');
         this._disabled = false
     }
     _init(){
-        this._matrixes.forEach((matrix,ind) => {
+        this._matrixes.forEach((matrix) => {
             //todo: add operation
             matrix.create().getHtml().prependTo(this._$calcField)
         })
@@ -200,11 +190,11 @@ class MatrixControl {
     _calc(){
         let matrixesAsArray = this._matrixes.map(matrix => {
             return matrix.readFromTable()
-        })
-        let operator = new Operator(this._operator).getValue()
-        let array = Operational[operator](matrixesAsArray)
-        let height = array.length
-        let width = array[0].length
+        });
+        let operator = new Operator(this._operator).getValue();
+        let array = Operational[operator](matrixesAsArray);
+        let height = array.length;
+        let width = array[0].length;
         new OutputMatrix(width, height)
             .create()
             .writeToTable(array)
@@ -212,8 +202,8 @@ class MatrixControl {
             .appendTo(this._$resultField.empty())
     }
 }
-let m1 = new InputMatrix(3, 2)
-let m2 = new InputMatrix(3, 2)
-//todo: 3 and more matrixes
+let m1 = new InputMatrix(3, 2);
+let m2 = new InputMatrix(3, 2);
+// todo: 3 and more matrixes
 
-let MainPage = new MatrixControl([m1,m2],"+")
+let MainPage = new MatrixControl([m1,m2],"+");
