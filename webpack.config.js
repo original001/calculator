793af7a1384a37fed775110b01d890b9baaf7454
vladimir <wertyxa@bk.ru>
2015-12-06 23:55:50 +0500

    fix babel core error

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,10 @@ module.exports = {
             {
                 test:/\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
             },
             {
                 test:/\.css$/,
